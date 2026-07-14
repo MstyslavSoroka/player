73d5902665c629d500f02cb3cf85cdead8ded3ac
MstyslavSoroka <mstyslav2010@gmail.com>
2026-07-14 13:02:05 +0100

    create playlist

--- main.c
+++ main.c
@@ -2,24 +2,93 @@
 #include <gtk/gtk.h>
 #include <sqlite3.h>
 
-static void on_quit_clicked(GtkButton *button, gpointer user_data) {
-  GtkWindow *window = GTK_WINDOW(user_data);
-  gtk_window_destroy(window);
+
+typedef struct {
+    sqlite3 *db;
+} AppData;
+
+
+typedef struct {
+    sqlite3 *db;
+    GtkEntry *entry;
+} CreatePlaylistData;
+
+static void on_add_clicked(GtkButton *button, gpointer user_data) {
+    GtkPopover *popover = GTK_POPOVER(user_data);
+    gtk_popover_popup(popover);
+  }
+
+static void on_create_clicked(GtkButton *button, gpointer user_data)
+{
+      CreatePlaylistData *data = user_data;
+
+    const char *name =
+        gtk_editable_get_text(GTK_EDITABLE(data->entry));
+
+    sqlite3_stmt *stmt;
+
+    sqlite3_prepare_v2(
+        data->db,
+        "INSERT INTO playlists(name) VALUES(?);",
+        -1,
+        &stmt,
+        NULL
+    );
+
+    sqlite3_bind_text(stmt, 1, name, -1, SQLITE_TRANSIENT);
+    sqlite3_step(stmt);
+     g_print("Created playlist: %s\n", name);
+    sqlite3_finalize(stmt);
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
+   AppData *app_data = user_data;
+
   GtkBuilder *builder = gtk_builder_new();
 
   GError *error = NULL;
   gtk_builder_add_from_file(builder, "app.ui", &error);
 
   GtkWindow *window = GTK_WINDOW(gtk_builder_get_object(builder, "window"));
-  GtkWidget *quit_btn = GTK_WIDGET(gtk_builder_get_object(builder, "quit"));
+  GtkWidget *add_playlist = GTK_WIDGET(gtk_builder_get_object(builder, "add_playlist"));
 
   gtk_window_set_application(window, app);
-  gtk_window_set_title(window, "Player");
+    gtk_window_set_title(window, "Player");
+    gtk_window_set_default_size(window, 1200, 800);
 
-  g_signal_connect(quit_btn, "clicked", G_CALLBACK(on_quit_clicked), window);
+
+  GtkWidget *playlist_window = gtk_popover_new();
+  gtk_widget_set_parent(playlist_window, add_playlist);
+
+  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
+    gtk_popover_set_child(GTK_POPOVER(playlist_window), box);
+
+    gtk_box_append(GTK_BOX(box),
+        gtk_label_new("Create new playlist"));
+    
+    GtkWidget *playlist_name = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(playlist_name), "Playlist name");
+    GtkWidget *create = gtk_button_new_with_label("Create");
+   
+
+
+    gtk_box_append(GTK_BOX(box),playlist_name);
+    gtk_box_append(GTK_BOX(box),create);
+
+  CreatePlaylistData *create_data =
+        g_new(CreatePlaylistData, 1);
+
+   create_data->db = app_data->db;
+    create_data->entry = GTK_ENTRY(playlist_name);
+ 
+    g_signal_connect(
+        create,
+        "clicked",
+        G_CALLBACK(on_create_clicked),
+        create_data
+    );
+
+  g_signal_connect(add_playlist, "clicked", G_CALLBACK(on_add_clicked), playlist_window);
   gtk_window_present(window);
   g_object_unref(builder);
 }
@@ -28,9 +97,8 @@ int main(int argc, char **argv) {
 #ifdef GTK_SRCDIR
   g_chdir(GTK_SRCDIR);
 #endif
-
+ 
   sqlite3 *db;
-
   if (sqlite3_open("./db/playlists.db", &db) != SQLITE_OK) {
     printf("Failed to open database: %s\n", sqlite3_errmsg(db));
     return 1;
@@ -49,14 +117,27 @@ int main(int argc, char **argv) {
     &err
   );
 
-  GtkApplication *app;
-  int status;
-  app = gtk_application_new("org.player.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
+  AppData app_data = {
+        .db = db
+    };
+
+GtkApplication *app;
+int status;
+
+app = gtk_application_new(
+    "org.player.example",
+    G_APPLICATION_DEFAULT_FLAGS
+);
+
+g_signal_connect(
+    app,
+    "activate",
+    G_CALLBACK(activate),
+    &app_data
+);  status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-
+ sqlite3_close(db);
 
   return status;
 }
